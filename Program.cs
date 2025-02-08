9f20eb32c5251aa6930f16d1eecfde4fb502c5c7
AndriiIhnatuk <ipz231_iam@student.ztu.edu.ua>
2025-02-08 15:20:43 +0200

    1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введiть перше число: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введiть друге число: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double sum = num1 + num2;
+        Console.WriteLine($"Сума: {sum}");
+    }
+}
